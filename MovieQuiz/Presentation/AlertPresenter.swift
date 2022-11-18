053c193a3bbe65550629a736bc51cdb509b2222a
MaximAgarev <maximagarev@yandex.ru>
2022-11-19 02:04:02 +0400

    Спринт 5/9: 5 → Тема 2/4: Ответственность → Урок 7/7

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+class AlertPresenter: AlertPresenterProtocol {
+    
+    var result: AlertModel
+    
+    weak var delegate: AlertPresenterDelegate?
+    
+    func requestAlert() {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: result.buttonText,
+            style: .default,
+            handler: result.completion
+        )
+        
+        alert.addAction(action)
+        
+        delegate?.didReceiveAlert(alert: alert)
+    }
+    
+    init(result: AlertModel, delegate: AlertPresenterDelegate?) {
+        self.result = result
+        self.delegate = delegate
+    }
+    
+}
